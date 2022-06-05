8fb7d395f2fdc8b32741a727e41015238e1df791
shubhammodi <modi.shubham12@gmail.com>
2022-06-05 16:12:12 +0530

    test for NotInCols corrected

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -72,6 +72,12 @@ def api_response(dict_request):
             response = predict(data)
             response = {"response":response}
             return response
-    except Exception as e:
+    except NotInRange as e:
         response = {"the_expected_range":get_schema(),"response": str(e)}
+        return response
+    except NotInCols as e:
+        response = {"the_expected_cols":list(get_schema().keys()),"response": str(e)}
+        return response
+    except Exception as e:
+        response = {"response": str(e)}
         return response
